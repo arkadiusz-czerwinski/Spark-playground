1840812535eb5ac9261a7c986ee31f7729a0cb8d
Arkadiusz Czerwiński <42278982+arkadiusz-czerwinski@users.noreply.github.com>
2021-02-21 19:45:51 +0000

    test

--- job_analysis/scrap.py
+++ job_analysis/scrap.py
@@ -2,8 +2,9 @@ user = 'dev'
 password = 'dev'
 import pymongo
 client = pymongo.MongoClient(f'mongodb+srv://{user}:{password}@cluster0.pjvh9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client.test
+db = client.playground
 mycol = db.job_offers
-mydict = { "name": "John", "address": "Highway 37" }
+# Tutaj scripting
+# mydict = { "name": "John", "address": "Highway 37" }
 x = mycol.insert_one(mydict)
 
